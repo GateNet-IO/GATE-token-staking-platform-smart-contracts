b727bfea8cb8b7d04f63c829107735b52821fcaa
frehml <frederic.hamelink@hotmail.com>
2021-12-22 08:04:34 +0100

    fee distribution

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -7,27 +7,27 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Staking is Ownable {
     struct Stake {
         uint256 amount;
-        uint256 unstaked;
         uint256 stakeTime;
-        uint256 reward;
-    }
-
-    struct Fee {
-        uint256 perStake;
-        uint256 time;
+        uint256 fee;
     }
 
     uint256 minimumStake = 1000 * 1e18;
     uint256 lockPeriod = 31 days;
 
     uint256 public totalStaked; // total amount of tokens staked
-    uint256 public rewardPerDay; // token rewards per block
+    uint256 public rewardRate; // token rewards per day
     uint256 public beginDate; // start date of rewards
     uint256 public endDate; // end date of rewards
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
+    uint256 public feePerToken;
     address public compound; // compound contract address
     IERC20 public stakedToken; // token allowed to be staked
-    mapping(address => Stake[]) stakes;
-    Fee[] public fees;
+
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
+    mapping(address => uint256) public fees;
+    mapping(address => Stake[]) public stakes;
 
     event Staked(address indexed user, uint256 amount);
     event Claimed(address indexed user, uint256 amount);
@@ -47,57 +47,70 @@ contract Staking is Ownable {
         endDate = _endDate;
     }
 
+    modifier started() {
+        require(block.timestamp >= beginDate);
+        _;
+    }
+
     modifier onlyCompound() {
         require(msg.sender == compound, "Staking:: Only compound");
         _;
     }
 
     function feeDistribution(uint256 amount) external onlyOwner {
-        fees.push(Fee(amount / totalStaked, block.timestamp));
+        feePerToken += amount / totalStaked;
+        stakedToken.transferFrom(msg.sender, address(this), amount);
         emit FeeDistributed(block.number, amount);
     }
 
-    function stake(uint256 amount) external {
+    function stake(uint256 amount)
+        external
+        started
+        distributeReward(msg.sender)
+    {
         require(amount >= minimumStake, "Stake too small");
         totalStaked += amount;
-        stakes[msg.sender].push(Stake(amount, 0, block.timestamp, 0));
+        stakes[msg.sender].push(Stake(amount, block.timestamp, feePerToken));
         stakedToken.transferFrom(msg.sender, address(this), amount);
         emit Staked(msg.sender, amount);
     }
 
-    function claim() external {
-        //emit Claimed(msg.sender, pending + feeReward);
-    }
+    function claim() external started distributeReward(msg.sender) {
+        uint256 reward = rewards[msg.sender];
 
-    function unstake(uint256 amount, uint256 index) public {
-        require(
-            stakes[msg.sender][index].amount >= amount,
-            "Not enough staked tokens"
-        );
-        require(
-            stakes[msg.sender][index].stakeTime + lockPeriod <= block.timestamp,
-            "Stake needs to be locked for a minimum period of time"
-        );
-
-        uint256 trueEnd;
-        if (block.timestamp >= endDate) {
-            trueEnd = endDate;
-        } else {
-            trueEnd = block.timestamp;
+        for (uint256 i = 0; i < stakes[msg.sender].length; i++) {
+            reward += stakes[msg.sender][i].amount * (feePerToken - stakes[msg.sender][i].fee);
+            stakes[msg.sender][i].fee = feePerToken;
         }
 
-        stakes[msg.sender][index].reward +=
-            amount *
-            getReward(stakes[msg.sender][index].stakeTime, block.timestamp);
-        stakes[msg.sender][index].amount -= amount;
+        reward += fees[msg.sender];
+        fees[msg.sender] = 0;
 
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            stakedToken.transfer(msg.sender, reward);
+            emit Claimed(msg.sender, reward);
+        }
+    }
+
+    function unstake(uint256 amount, uint256 index)
+        public
+        started
+        distributeReward(msg.sender)
+    {
+        require(amount > 0, "Cannot unstake 0");
+        require(amount <= stakes[msg.sender][index].amount, "Stake too big");
+        require(
+            stakes[msg.sender][index].stakeTime + lockPeriod >= block.timestamp
+        );
         totalStaked -= amount;
-
-        stakedToken.transferFrom(address(this), msg.sender, amount);
+        stakes[msg.sender][index].amount -= amount;
+        fees[msg.sender] += amount * (feePerToken - stakes[msg.sender][index].fee);
+        stakedToken.transferFrom(msg.sender, address(this), amount);
         emit Unstaked(msg.sender, amount, index);
     }
 
-    function unstakeAll() external {
+    function unstakeAll() external started {
         for (uint256 i = 0; i < stakes[msg.sender].length; i++) {
             if (
                 stakes[msg.sender][i].stakeTime + lockPeriod <= block.timestamp
@@ -107,38 +120,49 @@ contract Staking is Ownable {
         }
     }
 
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        return block.timestamp < endDate ? block.timestamp : endDate;
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (totalStaked == 0) {
+            return rewardPerTokenStored;
+        }
+        return
+            rewardPerTokenStored +
+            (lastTimeRewardApplicable() -
+                ((lastUpdateTime) * (rewardRate) * (1e18)) /
+                (totalStaked));
+    }
+
     function autoCompStake(uint256 _amount) external onlyCompound {}
 
     function autoCompUnstake(uint256 _amount) external onlyCompound {}
 
-    function getReward(uint256 from, uint256 to) public view returns (uint256) {
-        return to - from * 1 days * rewardPerDay;
-    }
-
-    function pendingReward(address _user) public view returns (uint256) {
+    function pendingReward(address user) public view returns (uint256) {
         uint256 amount;
-        for (uint256 i = 0; i < stakes[msg.sender].length; i++) {
-            amount +=
-                stakes[msg.sender][i].amount *
-                getReward(block.timestamp, stakes[msg.sender][i].stakeTime);
-            amount += stakes[msg.sender][i].reward;
+
+        for (uint256 i = 0; i < stakes[user].length; i++) {
+            amount += stakes[user][i].amount;
         }
+        return
+            (amount * (rewardPerToken() - (userRewardPerTokenPaid[user]))) /
+            (1e18) +
+            (rewards[user]);
     }
 
     function pendingFee(address user) public view returns (uint256) {
-        uint256 amount;
-        for (uint256 x = 0; x < fees.length; x++) {
-            for (uint256 y = 0; y < stakes[msg.sender].length; y++) {
-                if (fees[x].time < stakes[msg.sender][y].stakeTime) {
-                    amount += stakes[msg.sender][y].amount * fees[x].perStake;
-                    amount += stakes[msg.sender][y].unstaked * fees[x].perStake;
-                }
-            }
-        }
-        return amount;
     }
 
-    function distributeReward() public {}
+    modifier distributeReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (account != address(0)) {
+            rewards[account] = pendingReward(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
+        _;
+    }
 
     function setCompoundAddress(address _compound) public onlyOwner {
         compound = _compound;
@@ -148,8 +172,6 @@ contract Staking is Ownable {
         public
         onlyOwner
     {}
-
-    function safeRewardTransfer(address _to, uint256 _amount) internal {}
 
     function getUserStakes(address user) public view returns (Stake[] memory) {
         return stakes[user];
